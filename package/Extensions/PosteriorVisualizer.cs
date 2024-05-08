569897571829982d635fa8d90456c1b0fe666a97
Joaquin Rapela <joacorapela@gmail.com>
2024-05-08 21:15:18 +0100

    Added

---
+++ package/Extensions/PosteriorVisualizer.cs
@@ -0,0 +1,84 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Drawing;
+using Bonsai.Design;
+using System.Windows.Forms;
+using Bonsai.Design.Visualizers;
+using MathNet.Numerics;
+using ScottPlot;
+using ScottPlot.Plottable;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Sink)]
+[TypeVisualizer(typeof(PosteriorVisualizer))]
+public class PosteriorVisualizer : DialogTypeVisualizer
+{
+    private static Heatmap _hm;
+    private static double[,] _buffer;
+
+    public PosteriorVisualizer()
+    {
+    }
+
+    public override void Load(IServiceProvider provider)
+    {
+        ScottPlot.FormsPlot formsPlot1;
+    	double[] x;
+    	double[] y;
+
+        formsPlot1 = new() { Dock = DockStyle.Fill };
+        x = MathNet.Numerics.Generate.LinearRange(-1.0, 0.01, 1.0);
+        y = MathNet.Numerics.Generate.LinearRange(-1.0, 0.01, 1.0);
+        _buffer = new double[x.Length, y.Length];
+
+        // Add sample data to the plot
+        _hm = _formsPlot1.Plot.AddHeatmap(_buffer, lockScales: false);
+        _hm.FlipVertically = true;
+        _hm.XMin = -1.0;
+        _hm.XMax = 1.0;
+        _hm.YMin = -1.0;
+        _hm.YMax = 1.0;
+        // _formsPlot1.Frameless();
+        formsPlot1.Refresh();
+
+        var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+        if (visualizerService != null)
+        {
+            visualizerService.AddControl(_hm);
+        }
+    }
+
+    public override void Show(object value)
+    {
+	PosteriorDataItem data_item = (PosteriorDataItem) value;
+        computeMultivariateGaussianPDForGrid(_buffer, data_item.mn.ToArray(), data_item.Sn.ToArray());
+        _hm.Update(_buffer);
+    }
+
+    public override void Unload()
+    {
+    }
+
+    private static void computeMultivariateGaussianPDForGrid(double[,] buffer, double[] mn, double[,] Sn)
+    {
+        Vector<double> mnVec = Vector<double>.Build.DenseOfArray(mn);
+        Matrix<double> SnMat = Matrix<double>.Build.DenseOfArray(Sn);
+        double[] eval_loc_buffer = new double[2];
+        MatrixNormal matrixNormal = new MatrixNormal(mnVec.ToColumnMatrix(), SnMat, Matrix<double>.Build.DenseIdentity(1));
+        for (int i = 0; i < _x.Length; i++)
+        {
+            eval_loc_buffer[0] = _x[i];
+            for (int j = 0; j < _y.Length; j++)
+            {
+                eval_loc_buffer[1] = _y[j];
+                Vector<double> eval_loc = Vector<double>.Build.Dense(eval_loc_buffer);
+                buffer[j, i] = matrixNormal.Density(eval_loc.ToColumnMatrix());
+            }
+        }
+    }
+}
